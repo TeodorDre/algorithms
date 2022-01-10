7478dfb52eeedba5f42bb36b4f5c1e8a3c13b7d1
TeodorDre <swen295@gmail.com>
2022-01-10 19:56:19 +0300

    feat(quicksort): finalize quicksort

--- list/quick-sort/quick-sort.cpp
+++ list/quick-sort/quick-sort.cpp
@@ -3,18 +3,49 @@
 
 using namespace std;
 
+void PrintVector(const vector<int> &elements) {
+    for (const auto &i: elements) {
+        cout << i << endl;
+    }
+}
+
 vector<int> QuickSort(vector<int> &elements) {
-    return elements;
+    if (elements.size() < 2) {
+        return elements;
+    }
+
+    int first = elements.front();
+    elements.erase(begin(elements));
+
+    vector<int> less;
+    vector<int> greater;
+
+    for (const auto &el: elements) {
+        if (el < first) {
+            less.push_back(el);
+        } else {
+            greater.push_back(el);
+        }
+    }
+
+    vector<int> final_vector;
+
+    vector<int> sorted_less = QuickSort(less);
+    vector<int> sorted_greater = QuickSort(greater);
+
+    final_vector.insert(final_vector.end(), sorted_less.begin(), sorted_less.end());
+    final_vector.push_back(first);
+    final_vector.insert(final_vector.end(), sorted_greater.begin(), sorted_greater.end());
+
+    return final_vector;
 }
 
 int main() {
-    vector<int> numbers = {15, 7, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 2};
+    vector<int> numbers = {3, 5, 2, 1, 4};
 
     vector<int> sorted_numbers = QuickSort(numbers);
 
-    for (const auto &i: sorted_numbers) {
-        cout << i << endl;
-    }
+    PrintVector(sorted_numbers);
 
     return 0;
 }
--- main.cpp
+++ main.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
-#include <math.h>
+#include <vector>
 
 using namespace std;
+
+void PrintVector(const vector<int> &elements) {
+    for (const auto &i: elements) {
+        cout << i << endl;
+    }
+}
 
 int main() {
     return 0;
