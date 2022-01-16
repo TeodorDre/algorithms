1491c693f89158cb67d71794bddd7077b908fb79
TeodorDre <swen295@gmail.com>
2022-01-16 22:08:07 +0300

    feat(graph): add assets

--- main.cpp
+++ main.cpp
@@ -1,14 +1,65 @@
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
-void PrintVector(const vector<int> &elements) {
-    for (const auto &i: elements) {
-        cout << i << endl;
-    }
-}
+struct Man {
+    string name = "Man";
+    bool is_dating = false;
+    vector<string> friends_name;
+};
 
 int main() {
+    map<string, Man> friends;
+
+    friends["andrew"] = {
+            "andrew",
+            false,
+            {"alice", "bob", "claire"}
+    };
+
+    friends["bob"] = {
+            "bob",
+            false,
+            {"anuj", "peggy"}
+    };
+
+    friends["alice"] = {
+            "alice",
+            false,
+            {"peggy"}
+    };
+
+    friends["claire"] = {
+            "claire",
+            false,
+            {"thom", "johny"}
+    };
+
+    friends["anuj"] = {
+            "anuj",
+            false,
+            {"andrew"}
+    };
+
+    friends["johny"] = {
+            "johny",
+            false,
+            {}
+    };
+
+    friends["thom"] = {
+            "thom",
+            false,
+            {}
+    };
+
+    friends["peggy"] = {
+            "peggy",
+            true,
+            {}
+    };
+
     return 0;
 }
